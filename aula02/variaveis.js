eae4324e5e009f91c807590b1f9fe9c592ab9112
susi2432 <s.iris.pastorello@uol.com.br>
2023-03-22 08:29:06 -0300

    variaveis e tipos

---
+++ aula02/variaveis.js
@@ -0,0 +1,25 @@
+
+let num1 = 4
+let teste = false
+const num2 = 3
+var msg = "dando de"
+
+console.log(msg, num1)
+console.log(num1 + num2)
+console.log(typeof num1)
+console.log(typeof msg)
+
+//JSON - JavaScript Object Notation
+
+let usuario = {
+    nome: "Lucas",
+    idade: "69",
+    signo: "aries",
+    calvo: false
+}
+console.log(usuario.nome)
+console.log("nome", usuario.nome, "/n")
+console.log(typeof usuario.calvo)
+
+const nomes = ["João, Oãoj"]
+console.log(nomes)
